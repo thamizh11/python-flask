ee45c5e212fab1fc8359caa662b3275e5f43507b
thamizh <thamizh112000@gmail.com>
2026-02-06 12:26:11 +0530

    che devfile bug fix-2

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-#python app 
-
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Eclipse Che + Flask!"
+def home():
+    return "Hello from Eclipse Che Python Workspace!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
